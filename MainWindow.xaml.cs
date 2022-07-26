5e3ab2f55802a4245631cd91eaa8688aa1686146
HughTB <hugh.baldwin71@gmail.com>
2022-07-26 13:04:34 +0100

    Add configuration

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 using System.Globalization;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace Wallpapering
 {
@@ -24,6 +26,9 @@ namespace Wallpapering
     {
         private bool invertClock = false;
         private bool twelveHr = false;
+        private string background = "background.jpg";
+        private int buttonSize = 100;
+        private List<ConfigButton> buttons = new List<ConfigButton>();
 
         public MainWindow()
         {
@@ -40,24 +45,102 @@ namespace Wallpapering
 
         private void UpdateClock()
         {
+            lblClock.Foreground = (invertClock) ? Brushes.Black : Brushes.White;
+            lblDate.Foreground = (invertClock) ? Brushes.Black : Brushes.White;
             lblClock.Content = DateTime.Now.ToString((twelveHr) ? @"hh:mm:ss tt" : @"HH:mm:ss", CultureInfo.CurrentCulture);
             lblDate.Content = DateTime.Now.ToString(@"dddd, d MMMM yyyy", CultureInfo.CurrentCulture);
         }
 
         private void LoadConfig()
         {
+            if (File.Exists("config.json")) {
+                Config conf;
 
+                using (StreamReader sr = new StreamReader("config.json"))
+                {
+                    conf = JsonConvert.DeserializeObject<Config>(sr.ReadToEnd());
+                }
+
+                twelveHr = conf.twelveHr;
+                invertClock = conf.invertClock;
+                background = conf.background;
+                buttonSize = conf.buttonSize;
+                buttons = conf.buttons;
+
+                meBackground.Source = new Uri($"{Directory.GetCurrentDirectory()}\\{System.IO.Path.GetFileName(background)}", UriKind.Absolute);
+                UpdateButtons();
+            } else
+            {
+                SaveConfig();
+                UpdateButtons();
+                meBackground.Source = new Uri($"{Directory.GetCurrentDirectory()}\\{System.IO.Path.GetFileName(background)}", UriKind.Absolute);
+            }
         }
 
         private void SaveConfig()
         {
+            Config conf = new Config()
+            {
+                twelveHr = this.twelveHr,
+                invertClock = this.invertClock,
+                background = this.background,
+                buttonSize = this.buttonSize,
+                buttons = this.buttons
+            };
 
+            using (StreamWriter sw = new StreamWriter("config.json"))
+            {
+                sw.WriteLine(JsonConvert.SerializeObject(conf, Formatting.Indented));
+            }
+        }
+
+        private void UpdateButtons()
+        {
+            wrpButtons.Children.Clear();
+
+            foreach (ConfigButton configButton in buttons)
+            {
+                Button button = new Button();
+                button.Content = "";
+                button.Click += delegate (object sender, RoutedEventArgs e)
+                {
+                    System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo()
+                    {
+                        Arguments = configButton.uri,
+                        FileName = "explorer.exe"
+                    });
+                };
+                button.Width = buttonSize;
+                button.Height = buttonSize;
+                button.Margin = new Thickness(10, 0, 10, 0);
+                button.BorderThickness = new Thickness(0);
+                button.Background = Brushes.Transparent;
+                button.Content = new Image()
+                {
+                    Source = new BitmapImage(new Uri($"{Directory.GetCurrentDirectory()}\\icons\\{configButton.icon}", UriKind.Absolute)),
+                    Stretch = Stretch.UniformToFill,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    HorizontalAlignment = HorizontalAlignment.Center
+                };
+
+                wrpButtons.Children.Add(button);
+            }
+        }
+
+        private void UpdateWallpaper(string filename)
+        {
+            File.Copy(filename, $"{Directory.GetCurrentDirectory()}\\{System.IO.Path.GetFileName(filename)}");
+            meBackground.Source = new Uri($"{Directory.GetCurrentDirectory()}\\{System.IO.Path.GetFileName(filename)}", UriKind.Absolute);
         }
 
         private void Main_Loaded(object sender, RoutedEventArgs e)
         {
+            LoadConfig();
+
             ResizeWindow();
             UpdateClock();
+
+            meBackground.Stretch = Stretch.UniformToFill;
 
             DispatcherTimer clockTimer = new DispatcherTimer(DispatcherPriority.Render);
             clockTimer.Tick += delegate (object? sender, EventArgs e)
@@ -66,9 +149,26 @@ namespace Wallpapering
             };
             clockTimer.Interval = new TimeSpan(0, 0, 0, 0, 1000);
             clockTimer.Start();
-
-            lblClock.Foreground = (invertClock) ? Brushes.White : Brushes.Black;
-            lblDate.Foreground = (invertClock) ? Brushes.White : Brushes.Black;
         }
+
+        private void Main_Closed(object sender, EventArgs e)
+        {
+            SaveConfig();
+        }
+    }
+
+    struct Config
+    {
+        public bool invertClock;
+        public bool twelveHr;
+        public string background;
+        public int buttonSize;
+        public List<ConfigButton> buttons;
+    }
+
+    struct ConfigButton
+    {
+        public string uri;
+        public string icon;
     }
 }
